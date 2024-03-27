161a2b2bc0658eb2b4d80e2fabb5cedda962eb8d
Ny Antsa <nyantsa.rajaonarison@gmail.com>
2024-03-27 13:56:14 -0400

    Display Portfolio Experience Tab

--- force-app/main/default/lwc/portfolioWorkExperienceTab/portfolioWorkExperienceTab.js
+++ force-app/main/default/lwc/portfolioWorkExperienceTab/portfolioWorkExperienceTab.js
@@ -3,6 +3,7 @@ import { getRelatedListRecords } from 'lightning/uiRelatedListApi';
 
 export default class PortfolioWorkExperienceTab extends LightningElement {
     @api recordId; 
+    workExperienceList = [];
 
     @wire( getRelatedListRecords, {
         parentRecordId: '$recordId',
@@ -17,9 +18,33 @@ export default class PortfolioWorkExperienceTab extends LightningElement {
     }) workExperienceHandler({data, error}) {
         if(data) {
             console.log('WorkingExperience Data', JSON.stringify(data));
+            this.formatExperience(data);
         }
         if(error) {
             console.error(error);
         }
+    }
+
+    formatExperience(data) {
+        this.workExperienceList = [...data.records].reverse().map(item  => {
+            let id = item.id;
+            const {Job_Start_Date__c, Company_Name__c, Description__c, Is_Current__c, Job_End_Date__c, Title__c, Work_Location__c} = item.fields;
+
+            let JobStartDate = this.getValue(Job_Start_Date__c);
+            let CompanyName = this.getValue(Company_Name__c);
+            let Description = this.getValue(Description__c);
+            let IsCurrent = this.getValue(Is_Current__c);
+            let JobEndDate = this.getValue(Job_End_Date__c);
+            let Title = this.getValue(Title__c);
+            let WorkLocation = this.getValue(Work_Location__c);
+
+            return {id, JobStartDate, CompanyName, Description, IsCurrent, JobEndDate, Title, WorkLocation};
+        });
+
+        console.log("This is work Experience List:", JSON.stringify(this.workExperienceList));
+    }
+
+    getValue(data) {
+        return data && (data.displayValue || data.value);
     }
 }
